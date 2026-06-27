ee40201d124c90b704d350b4b7403f2b62eb3b3d
anoddyne <besshaposhnikovv@gmail.com>
2026-06-27 18:46:41 +0300

    Initial commit

---
+++ src/test/java/ru/neoflex/practice/credit_service/CreditServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.neoflex.practice.credit_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
